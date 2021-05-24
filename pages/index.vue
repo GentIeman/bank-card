2da914828bc9cae1d3ff790259ec0cb178a3688a
Gentleman <gentleman-2021@mail.ru>
2021-05-25 00:15:40 +0300

    Fix css styles

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,12 @@ export default {}
 </script>
 
 <style lang="stylus">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+}
 .container {
   width 100%
   height 100vh
