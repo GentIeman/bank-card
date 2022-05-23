6c3d10ad388c173227253a28cf5134d77f2093d0
gentIeman <gentleman-2021@mail.ru>
2022-05-23 21:20:21 +0300

    update: removed unused code

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,8 @@
 <template>
-  <div class="page">
+  <div>
     <Nuxt />
   </div>
 </template>
 
-<style lang="stylus">
-:where(*) {
-  margin 0
-  padding 0
-  box-sizing border-box
-}
-
-.page {
-  display flex
-  justify-content center
-  align-items center
-  width 100%
-  min-height 100vh
-}
+<style>
 </style>
